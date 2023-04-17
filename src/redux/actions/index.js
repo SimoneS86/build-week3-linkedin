a246e8e59642c6b8ade5f6261bd412ea59be86f7
SimoneS86 <simone.sensini.86@gmail.com>
2023-04-17 16:33:10 +0200

    step2

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,14 +1,15 @@
 export const ADD_PROFILE = "ADD_PROFILE";
 
-export const getProfileAction = (prop) => {
+export const getProfileAction = () => {
   return async (dispatch) => {
-    const endpoint = "https://striveschool-api.herokuapp.com/api/profile/" + prop;
+    const endpoint = "https://striveschool-api.herokuapp.com/api/profile/5fc4af46b708c200175de88f";
     const fetchOpt = {
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNkMTMyMDIyYTZhYjAwMTQxYTg1NjciLCJpYXQiOjE2ODE3MjQxOTIsImV4cCI6MTY4MjkzMzc5Mn0.x8dPST_iOah2_5aT7ZuitZWbm0q-dOuBjsrQ8N4_VJI",
       },
     };
+    console.log(endpoint);
     try {
       const resp = await fetch(endpoint, fetchOpt);
       if (resp.ok) {
