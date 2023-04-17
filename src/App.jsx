d68555124a56171bd964afe6c12d2676e085bd43
SimoneS86 <simone.sensini.86@gmail.com>
2023-04-17 15:33:21 +0200

    step1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import './App.css';
+import MyHomePage from './Components/MyHomePage';
 
 function App() {
   return (
     <div className="App">
+      <MyHomePage prop="5fc4af46b708c200175de88f"/>
     </div>
   );
 }
