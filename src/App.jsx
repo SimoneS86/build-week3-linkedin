4c9af489441af573869d22af4eef4d96c00dceed
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-04-17 13:05:24 +0200

    fisrt commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import TopNav from "./components/TopNav";
 
 function App() {
   return (
     <div className="App">
+      <TopNav />
     </div>
   );
 }
