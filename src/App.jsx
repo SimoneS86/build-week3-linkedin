4c2cd6c267d28c0b8445cf9580fa2b15282b1ecd
Giuseppe Canzoneri <gcanzoneri6@gmail.com>
2023-04-18 17:02:14 +0200

    step-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import TopNav from "./components/TopNav";
-import Profile from "./components/Profile";
+import MyHomePage from "./Components/MyHomePage";
+import LinkedinFooter from "./Components/LinkedinFooter";
+import TopNav from "./Components/TopNav";
 
 function App() {
   return (
-    <div className="App">
-      <TopNav />
-      <Profile />
+    <div className="App sfondo">
+      <BrowserRouter>
+        <TopNav />
+        <Routes>
+          <Route path="/" element={<MyHomePage prop="5fc4af46b708c200175de88f" />} />
+        </Routes>
+        <LinkedinFooter />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/redux/actions/index.js
@@ -0,0 +1,25 @@
+export const ADD_PROFILE = "ADD_PROFILE";
+
+export const getProfileAction = prop => {
+  return async dispatch => {
+    const endpoint = "https://striveschool-api.herokuapp.com/api/profile/" + prop.prop;
+    const fetchOpt = {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDNkMTMyMDIyYTZhYjAwMTQxYTg1NjciLCJpYXQiOjE2ODE3MjQxOTIsImV4cCI6MTY4MjkzMzc5Mn0.x8dPST_iOah2_5aT7ZuitZWbm0q-dOuBjsrQ8N4_VJI",
+      },
+    };
+    console.log(endpoint);
+    try {
+      const resp = await fetch(endpoint, fetchOpt);
+      if (resp.ok) {
+        const dataProfile = await resp.json();
+        dispatch({ type: ADD_PROFILE, payload: dataProfile });
+      } else {
+        alert("Errore qualcosa è andato storto");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
