057dc81dcdbba7ee6b89e3d3168659c52f8faacc
SimoneS86 <simone.sensini.86@gmail.com>
2023-04-21 12:47:03 +0200

    Revert "Footer"
    This reverts commit 3ffe145b269627921c943fe1e0e1ea3960d76ce2.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LinkedinFooter from './Components/LinkedinFooter'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LinkedinFooter />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+    </div>
   );
 }
 
